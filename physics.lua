03a412daeb865bab7e42555b1e2d23eaa3247611
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2025-01-03 17:24:00 +0100

    Fine tune physcis around orbiting

--- physics.lua
+++ physics.lua
@@ -12,7 +12,7 @@ local function getGravity(centralBody, satelliteBody)
 end
 
 function _M.updateSatellite(satellite, bodies, dt)
-  local direction = satellite.velocity:normalise()
+  local direction = satellite.turnDirection
 
   local pull = b.vec2()
   for _, body in ipairs(bodies) do
@@ -38,18 +38,27 @@ function _M.updateSatellite(satellite, bodies, dt)
       local orbitUnit = orbitVector:normalise()
       local approachAngle = orbitVector:angle_difference(satellite.velocity)
       local orbitAngle = orbitUnit:angle()
-      local orbitSpeed = math.max(100, satellite.velocity:length())
       local orbitDireciton = approachAngle >= 0 and 1 or -1
-      local surfaceDirection = orbitAngle + orbitDireciton * math.pi / 2
-      local orbitingVelocity = b.vec2(math.cos(surfaceDirection), math.sin(surfaceDirection)) * orbitSpeed
+      local surfaceAngle = orbitAngle + orbitDireciton * math.pi / 2
+      local surfaceDirection = b.vec2(math.cos(surfaceAngle), math.sin(surfaceAngle))
+      local orbitSpeed
+      if satellite.thrust > 0.1 then
+        orbitSpeed = math.max(math.sqrt(body.mass / distance), satellite.velocity:length())
+      else
+        orbitSpeed = math.sqrt(body.mass / distance) - 10
+      end
+      local orbitingVelocity = surfaceDirection * orbitSpeed
       local orbitingPosition = satellite.position + orbitingVelocity * dt
+      -- correction of orbiting position
+      local correction = (orbitingPosition - body.position):length() - body.radius - body.orbit
+      orbitingPosition = orbitingPosition:lerp(orbitingPosition + orbitUnit * correction, 0.2)
 
       local orbitingDistance = (orbitingPosition - body.position):length()
       local normalDistance = (newPosition - body.position):length()
 
       if orbitingDistance > normalDistance then
-        newVelocity = orbitingVelocity
-        newPosition = orbitingPosition
+        newVelocity = newVelocity:lerp(orbitingVelocity, 0.2)
+        newPosition = newPosition:lerp(orbitingPosition, 0.2)
       end
     end
   else
@@ -60,7 +69,7 @@ function _M.updateSatellite(satellite, bodies, dt)
       if body.orbitable and distance <= satellite.radius + body.radius + body.orbit then
         satellite.orbiting = true
         satellite.orbitingBody = bodyIndex
-        satellite.velocity = satellite.velocity * 0.8
+        satellite.velocity = math.sqrt(body.mass / distance)
         break
       end
     end
